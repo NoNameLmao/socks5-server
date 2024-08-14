5874476813a60b78ae7c0e2c4fa1bedabd6319ed
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2024-08-14 19:51:16 +0300

    update node-socks5-server to v1.1.0

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-require('colors');
-console.log('Starting the socks5 server...'.yellow);
-const node_socks5_server = require('node-socks5-server');
-const { port } = require('./config.json');
-const socks5server = node_socks5_server.createServer();
-socks5server.once('listening', () => {
-    console.log(`The socks5 proxy server is now listening on port "${port}"!`.green);
-    socks5server.on('connection', socket => {
+require('colors')
+
+console.log('Starting the socks5 server...'.yellow)
+const socks5 = require('node-socks5-server').createServer()
+socks5.once('listening', () => {
+    console.log(`The socks5 proxy server is now listening on port "${port}"!`.green)
+    socks5.on('connection', socket => {
         console.log(
             `[${new Date().toLocaleString().green}] A connection was established! Information:\n` +
             `   Local:\n` +
@@ -14,7 +13,7 @@ socks5server.once('listening', () => {
             `   Remote:\n` +
             `       IP: ${socket.remoteAddress.cyan} (${socket.remoteFamily.cyan})\n` +
             `       Port: ${socket.remotePort.toString().cyan}`
-        );
-    });
-});
-socks5server.listen(parseInt(port));
+        )
+    })
+})
+socks5.listen(parseInt(port))
