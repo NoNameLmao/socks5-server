6f603cc7221fc7d1747591ccb80402501931092e
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2024-08-14 19:53:05 +0300

    accidentally removed port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('colors')
 
 console.log('Starting the socks5 server...'.yellow)
 const socks5 = require('node-socks5-server').createServer()
+const { port } = require('./config.json')
 socks5.once('listening', () => {
     console.log(`The socks5 proxy server is now listening on port "${port}"!`.green)
     socks5.on('connection', socket => {
